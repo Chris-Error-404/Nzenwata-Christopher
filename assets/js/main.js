5b114302cca8e513ad3f866a79d045c239814951
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-06-16 12:57:50 +0100

    done with animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-//bos shadow scroll script
+//box shadow scroll script
 window.addEventListener('scroll', () => {
     const navbar = document.querySelector('nav.navbar');
     if (window.scrollY > 10) {
@@ -7,4 +7,21 @@ window.addEventListener('scroll', () => {
         navbar.classList.remove('scrolled');
     }
 });
+
+
+//Float hover script
+document.querySelectorAll('.float-hover').forEach((el) => {
+    const bg = el.querySelector('.bg');
+  
+    el.addEventListener('mouseenter', () => {
+      bg.style.transformOrigin = 'bottom center';
+      bg.style.transform = 'scaleY(1)';
+    });
+  
+    el.addEventListener('mouseleave', () => {
+      bg.style.transformOrigin = 'top center';
+      bg.style.transform = 'scaleY(0)';
+    });
+  });
+  
 
